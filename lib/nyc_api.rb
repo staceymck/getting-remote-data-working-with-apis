21f7b6d00946e3780ee1e7f5ae146d1b066d8698
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-24 09:26:22 -0600

    commit tree 0c44d5e8ec087da9df13c49d3663a9ed1caf418d

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,18 +1,31 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
+require 'pry'
  
 class GetPrograms
-
+  #constant to store API endpoint
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
   def get_programs
     uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
+    response = Net::HTTP.get_response(uri) #sends HTTP GET request
     response.body
   end
 
+  def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs = JSON.parse(self.get_programs)
+      binding.pry
+      programs.collect do |program|
+        program["agency"]  
+      end
+  end 
+
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+
+programs = GetPrograms.new
+puts programs.program_school.uniq 
